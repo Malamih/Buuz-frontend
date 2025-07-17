7e99cd7e9aa7196714a82860bca4e95cd15d4891
amer-wild11 <wild4bs@gmail.com>
2025-07-18 00:50:17 +0300

    dynamic metadata removed

--- app/(main)/clients/[id]/page.tsx
+++ app/(main)/clients/[id]/page.tsx
@@ -1,24 +1,5 @@
 // app/portoflios/[id]/page.tsx
 import { Content } from "./Content";
-import { BASE_URL } from "@/lib/apiClient";
-
-export const generateMetadata = async ({
-  params,
-}: {
-  params: { id: string };
-}) => {
-  const res = await fetch(`${BASE_URL}/portoflios/${params.id}`);
-  const data = await res.json();
-  return {
-    title: `${data?.payload?.name} - BEEZ PRODUCTIONS`,
-    description: `${data?.description}`,
-    openGraph: {
-      title: data?.project?.title,
-      description: `${data?.description}`,
-      images: [data?.logo],
-    },
-  };
-};
 
 export default function Page() {
   return <Content />;
